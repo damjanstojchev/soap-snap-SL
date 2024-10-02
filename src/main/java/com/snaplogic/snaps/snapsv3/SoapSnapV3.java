4ebf3592e6429d1b5f8722011bb965d3bbb70cb1
Damjan Stojchev <damjans@baselinked.com>
2024-10-02 16:27:01 +0200

    Works, gives output but hardcoded

--- src/main/java/com/snaplogic/snaps/snapsv3/SoapSnapV3.java
+++ src/main/java/com/snaplogic/snaps/snapsv3/SoapSnapV3.java
@@ -7,13 +7,23 @@ import com.snaplogic.api.Snap;
 import com.snaplogic.common.properties.builders.PropertyBuilder;
 import com.snaplogic.snap.api.*;
 import com.snaplogic.snap.api.capabilities.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.json.XML;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @General(title = "Soap Snap v3", purpose = "Soap Requests", author = "Baselinked")
 @Inputs(min = 0, max = 1, accepts = {ViewType.DOCUMENT})
@@ -27,11 +37,13 @@ public class SoapSnapV3 implements Snap {
     private static final String SERVICE_NAME = "Service Name";
     private static final String ENDPOINT = "Endpoint";
     private static final String OPERATION = "Operation";
+    private static final String ENVELOPE = "Envelope";
 
     private static String wsdlUrl;
     private static String serviceName;
     private static String endPoint;
     private static String operation;
+    private static String envelope;
 
     private static final XmlHandlerImpl xmlHandler = new XmlHandlerImpl();
     @Inject
@@ -97,6 +109,7 @@ public class SoapSnapV3 implements Snap {
             wsdlUrl = propertyValues.get(WSDL_URL);
             serviceName = propertyValues.get(SERVICE_NAME);
             operation = propertyValues.get(OPERATION);
+            endPoint = propertyValues.get(ENDPOINT);
 
             Document document = xmlHandler.loadWsdl(wsdlUrl);
 
@@ -134,7 +147,167 @@ public class SoapSnapV3 implements Snap {
 
     @Override
     public void execute() throws ExecutionException, SnapDataException {
+//        String username = "SAAS.IMP2";
+//        String password = "welcome123";
+//
+//        try {
+//            // Create the credentials provider
+//            BasicCredentialsProvider credsProvider = new BasicCredentialsProvider();
+//            credsProvider.setCredentials(
+//                    new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT),
+//                    new UsernamePasswordCredentials(username, password)
+//            );
+//
+//            // Create HTTP client with credentials provider
+//            CloseableHttpClient httpClient = HttpClients.custom()
+//                    .setDefaultCredentialsProvider(credsProvider)
+//                    .build();
+//
+//            HttpPost httpPost = new HttpPost(WSDL_URL);
+//
+//            // SOAP request body
+//            String soapEnvelope = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+//                    + "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\""
+//                    + " xmlns:pub=\"http://xmlns.oracle.com/oxp/service/PublicReportService\">"
+//                    + "    <soap:Header/>"
+//                    + "    <soap:Body>"
+//                    + "        <pub:runReport>"
+//                    + "            <pub:reportRequest>"
+//                    + "                <pub:attributeFormat>csv</pub:attributeFormat>"
+//                    + "                <pub:parameterNameValues>"
+//                    + "                    <pub:item>"
+//                    + "                        <pub:name>pPERSON_NUMBER</pub:name>"
+//                    + "                        <pub:values>"
+//                    + "                            <pub:item>7</pub:item>"
+//                    + "                        </pub:values>"
+//                    + "                    </pub:item>"
+//                    + "                </pub:parameterNameValues>"
+//                    + "                <pub:reportAbsolutePath>/Custom/Person Details/Person Detail Report.xdo</pub:reportAbsolutePath>"
+//                    + "                <pub:sizeOfDataChunkDownload>-1</pub:sizeOfDataChunkDownload>"
+//                    + "            </pub:reportRequest>"
+//                    + "            <pub:appParams>?</pub:appParams>"
+//                    + "        </pub:runReport>"
+//                    + "    </soap:Body>"
+//                    + "</soap:Envelope>";
+//
+//            // Headers
+//            httpPost.setHeader("Content-Type", "application/soap+xml");
+//
+//            StringEntity entity = new StringEntity(soapEnvelope, "UTF-8");
+//            httpPost.setEntity(entity);
+//
+//            // Execute the request
+//            HttpResponse response = httpClient.execute(httpPost);
+//
+//            // Response status code
+//            int statusCode = response.getStatusLine().getStatusCode();
+//            System.out.println(statusCode);
+//
+//            // Get the response entity (the SOAP envelope)
+//            HttpEntity responseEntity = response.getEntity();
+//            if (responseEntity != null) {
+//                String responseString = EntityUtils.toString(responseEntity);
+//                //System.out.println(responseString); if the xml needs to be printed
+//
+//                // Convert the SOAP XML response to JSON
+//                JSONObject jsonResponse = XML.toJSONObject(responseString);
+//                System.out.println(jsonResponse.toString(4));
+//
+//                // Access specific fields in the JSON
+//                JSONObject envelope = jsonResponse.getJSONObject("env:Envelope");
+//                JSONObject body = envelope.getJSONObject("env:Body");
+//                // Access the response data from the body
+//                if (body.has("runReportResponse")) {
+//                    JSONObject reportResponse = body.getJSONObject("runReportResponse");
+//                    System.out.println("Report Response Data: " + reportResponse.toString());
+//                }
+//
+//            }
+//            httpClient.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        outputViews.write(documentUtility.newDocument("test123"));
 
+         BasicCredentialsProvider credsProvider;
+         CloseableHttpClient httpClient;
+         HttpPost httpPost;
+         String soapEnvelope;
+         StringEntity entity;
+         CloseableHttpResponse response;
+         String responseString;
+
+        try {
+            // Credentials setup
+            credsProvider = new BasicCredentialsProvider();
+            credsProvider.setCredentials(
+                    new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT),
+                    new UsernamePasswordCredentials("SAAS.IMP2", "welcome123")
+            );
+
+            // HTTP client setup
+            httpClient = HttpClients.custom()
+                    .setDefaultCredentialsProvider(credsProvider)
+                    .build();
+
+            // Prepare SOAP request
+            httpPost = new HttpPost(endPoint);
+
+            soapEnvelope = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                    + "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\""
+                    + " xmlns:pub=\"http://xmlns.oracle.com/oxp/service/PublicReportService\">"
+                    + "    <soap:Header/>"
+                    + "    <soap:Body>"
+                    + "        <pub:runReport>"
+                    + "            <pub:reportRequest>"
+                    + "                <pub:attributeFormat>csv</pub:attributeFormat>"
+                    + "                <pub:parameterNameValues>"
+                    + "                    <pub:item>"
+                    + "                        <pub:name>pPERSON_NUMBER</pub:name>"
+                    + "                        <pub:values>"
+                    + "                            <pub:item>7</pub:item>"
+                    + "                        </pub:values>"
+                    + "                    </pub:item>"
+                    + "                </pub:parameterNameValues>"
+                    + "                <pub:reportAbsolutePath>/Custom/Person Details/Person Detail Report.xdo</pub:reportAbsolutePath>"
+                    + "                <pub:sizeOfDataChunkDownload>-1</pub:sizeOfDataChunkDownload>"
+                    + "            </pub:reportRequest>"
+                    + "            <pub:appParams>?</pub:appParams>"
+                    + "        </pub:runReport>"
+                    + "    </soap:Body>"
+                    + "</soap:Envelope>";
+
+            entity = new StringEntity(soapEnvelope, "UTF-8");
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Content-Type", "application/soap+xml");
+
+            // Execute the SOAP request
+            response = httpClient.execute(httpPost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println("Response Status Code: " + statusCode);
+
+            // Check if response is successful
+            if (statusCode == 200) {
+                // Process the response
+                responseString = EntityUtils.toString(response.getEntity());
+
+                // Convert the SOAP XML response to JSON
+                JSONObject jsonResponse = XML.toJSONObject(responseString);
+
+                // Map to store output data
+                Map<String, String> data = new LinkedHashMap<>();
+                data.put("status_code", String.valueOf(statusCode));
+                data.put("response", jsonResponse.toString());
+                outputViews.write(documentUtility.newDocument(data));
+
+            } else {
+                System.out.println("Failed to get a valid response. Status Code: " + statusCode);
+            }
+            httpClient.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
